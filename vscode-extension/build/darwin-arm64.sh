ba665d8582a4002e9ed2de826d0a8365e563ae2e
LastLeaf <bqfu@163.com>
2025-02-07 16:41:02 +0800

    build: improve build scripts

--- vscode-extension/build/darwin-arm64.sh
+++ vscode-extension/build/darwin-arm64.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-# try to find a proper working directory
-if [[ ${PWD} == */vscode-extension/build ]]; then
-  cd ..
-  echo "Switch to vscode extension directory: ${PWD}"
-elif [[ ${PWD} == */vscode-extension ]]; then
-  echo "In vscode extension directory: ${PWD}"
-elif cd vscode-extension; then
-  echo "Switch to vscode extension directory: ${PWD}"
-else
-  echo "Cannot find a proper vscode-extension directory."
-  exit -1
-fi
+cd $(dirname "$0")
+cd ..
 
 source build/common/package.sh darwin-arm64 aarch64-apple-darwin --pre-release
--- vscode-extension/build/init.sh
+++ vscode-extension/build/init.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-# try to find a proper working directory
-if [[ ${PWD} == */vscode-extension/build ]]; then
-  cd ..
-  echo "Switch to vscode extension directory: ${PWD}"
-elif [[ ${PWD} == */vscode-extension ]]; then
-  echo "In vscode extension directory: ${PWD}"
-elif cd vscode-extension; then
-  echo "Switch to vscode extension directory: ${PWD}"
-else
-  echo "Cannot find a proper vscode-extension directory."
-  exit -1
-fi
+cd $(dirname "$0")
+cd ..
 
 mkdir -p packages
 rm packages/*.vsix
