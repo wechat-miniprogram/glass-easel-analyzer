39ceff55d4c420e62a2b9ca8bbaccc0c56bd6dc2
LastLeaf <bqfu@163.com>
2025-03-13 19:37:47 +0800

    feat: support LESS and SCSS as wxss diagnostic mode

--- vscode-extension/src/middleware.ts
+++ vscode-extension/src/middleware.ts
@@ -1,36 +1,44 @@
 import * as vscode from 'vscode'
 import path from 'node:path'
 import { type Middleware } from 'vscode-languageclient'
-import { getCSSLanguageService } from 'vscode-css-languageservice'
+import {
+  getCSSLanguageService,
+  getLESSLanguageService,
+  getSCSSLanguageService,
+  type LanguageService,
+} from 'vscode-css-languageservice'
 
-getCSSLanguageService()
+const cssLangService = getCSSLanguageService()
+const lessLangService = getLESSLanguageService()
+const scssLangService = getSCSSLanguageService()
 
 const getWxssDiagnostics = () =>
   vscode.workspace.getConfiguration('glass-easel-analyzer').get('wxssDiagnosticsMode') as string
 
-const cssLangService = getCSSLanguageService()
-
-const doCssValidation = async (uri: vscode.Uri) => {
+const doCssValidation = async (uri: vscode.Uri, ls: LanguageService) => {
   const doc = await vscode.workspace.openTextDocument(uri)
-  const sheet = cssLangService.parseStylesheet(doc as any)
-  return cssLangService.doValidation(doc as any, sheet)
+  const sheet = ls.parseStylesheet(doc as any)
+  return ls.doValidation(doc as any, sheet)
 }
 
 const middleware: Middleware = {
   handleDiagnostics(uri, diagnostics, next) {
     if (path.extname(uri.path) === '.wxss') {
       const mode = getWxssDiagnostics()
-      if (mode === 'disabled') {
-        next(uri, diagnostics)
-      } else {
+      if (mode === 'CSS' || mode === 'LESS' || mode === 'SCSS') {
+        let ls: LanguageService = cssLangService
+        if (mode === 'LESS') ls = lessLangService
+        else if (mode === 'SCSS') ls = scssLangService
         // eslint-disable-next-line @typescript-eslint/no-floating-promises
-        doCssValidation(uri)
+        doCssValidation(uri, ls)
           // eslint-disable-next-line promise/no-callback-in-promise
           .then((diag) => next(uri, diag as any))
           .catch(() => {
             // eslint-disable-next-line @typescript-eslint/no-floating-promises
             vscode.window.showErrorMessage('Failed to get CSS diagnostics')
           })
+      } else {
+        next(uri, diagnostics)
       }
       return
     }
